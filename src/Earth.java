706b5c103ec69eae02e9c401240bb8c40fffa46a
Vyacheslav Kurochkin <VKurochkin@epicloansystems.com>
2016-06-22 17:28:09 -0400

    Initial commit

---
+++ src/Earth.java
@@ -0,0 +1,15 @@
+/**
+ * Created by VKurochkin on 6/22/2016.
+ */
+public class Earth {
+
+    public static void main(String args[]){
+       Human human1 = new Human("Tom", 25, 76, "Blue");
+       Human human2 = new Human("Jim", 99, 76, "Blue");
+       Human human3 = new Human("Max", 25, 76, "Blue");
+
+       human1.speak();
+       human2.speak();
+       human3.speak();
+    }
+}
---
+++ src/Human.java
@@ -0,0 +1,37 @@
+/**
+ * Created by VKurochkin on 6/22/2016.
+ */
+public class Human {
+
+    String name;
+
+    public Human(String name, int age, int heightInInches, String eyeColor) {
+        this.name = name;
+        this.age = age;
+        this.heightInInches = heightInInches;
+        this.eyeColor = eyeColor;
+    }
+
+    int age;
+    int heightInInches;
+    String eyeColor;
+
+
+
+
+    public void speak(){
+        System.out.println("Hello my name is " + name);
+        System.out.println("I am " + heightInInches + " inches tall");
+        System.out.println("I am " + age + " old");
+        System.out.println("My eye color is " + eyeColor);
+
+    }
+
+    public void eat(){
+        System.out.println("eating ...");
+    }
+
+    public void walk(){
+        System.out.println("walking...");
+    }
+}
