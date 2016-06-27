b891adcaf2e8adbec4362ac7835cdc7c31b2c735
Slav Kurochkin <slavkurochkin@gmail.com>
2016-06-27 08:32:45 -0400

    Data Structure Queue

---
+++ src/learningdatastructure/queue/App.java
@@ -0,0 +1,18 @@
+package learningdatastructure.queue;
+
+/**
+ * Created by slavkurochkin on 6/26/16.
+ */
+public class App {
+
+    public static void main(String[] args) {
+        Queue myQueue = new Queue(5);
+        myQueue.insert(100);
+        myQueue.insert(300);
+        myQueue.insert(10);
+        myQueue.insert(1);
+        myQueue.insert(12);
+        myQueue.insert(22);
+        myQueue.view();
+    }
+}
